d75b3049f8799197a5dd25522c123503eb74be52
danalejos <sumigame98@gmail.com>
2023-04-15 12:50:16 -0500

    NT9 poo pyhton

---
+++ Heroe.py
@@ -0,0 +1,13 @@
+class Heroe:
+
+    #declaro la funcion especial constructor que se encarga de inicializar mis atributos
+    def __init__(self):
+        #defino los atributos de mi clase
+        #atributos=variables
+        self.nombre=None
+        self.edad=None
+        self.poderPatada=None
+        self.poderPuno=None
+
+    def saludar(self):
+        print("hola soy un heroe")
---
+++ main.py
@@ -0,0 +1,13 @@
+from Heroe import Heroe
+
+#para usar una clase debo sacarle copia=para crear una clase debo crear una instancia=para usar una clase debo crear un objeto
+#u objeto es una variable(especial)
+
+objeto=Heroe()
+
+#como uso el objeto?
+#1 llamando a sus atributos
+objeto.nombre="Goku"
+
+#2 llamando a los metodos de la clase
+objeto.saludar()
